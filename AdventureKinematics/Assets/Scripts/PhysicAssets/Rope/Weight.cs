027c6d029bdb636fc3608c826a4e7abb5ad2d2d4
NicolasIceberg <>
2020-07-11 20:44:39 +0300

    Error corrections. Improved Weight mechanics (Rope).

--- AdventureKinematics/Assets/Scripts/PhysicAssets/Rope/Weight.cs
+++ AdventureKinematics/Assets/Scripts/PhysicAssets/Rope/Weight.cs
@@ -3,13 +3,14 @@
 public class Weight : MonoBehaviour
 {
     public float DistanceFromRopeEnd;
-    
+
+    private DistanceJoint2D joint;
+
     public void ConnectToRopeEnd(Rigidbody2D rb)
     {
-        HingeJoint2D joint = gameObject.AddComponent<HingeJoint2D>();
+        joint = gameObject.GetComponent<DistanceJoint2D>();
         joint.autoConfigureConnectedAnchor = false;
         joint.connectedBody = rb;
-        joint.anchor = Vector2.zero;
-        joint.connectedAnchor = new Vector2(0f, - DistanseFromRopeEnd);
+        joint.distance = DistanceFromRopeEnd;
     }
 }
