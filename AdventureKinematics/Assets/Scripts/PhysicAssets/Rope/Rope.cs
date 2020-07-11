82b1ff3b0fc4f00784883195b5b926443cb39fce
NicolasIceberg <>
2020-07-11 22:11:48 +0300

    Improved Rope mechanics - added new ability to attach multiple ropes to single weight. Deleted Weight script.

--- AdventureKinematics/Assets/Scripts/PhysicAssets/Rope/Rope.cs
+++ AdventureKinematics/Assets/Scripts/PhysicAssets/Rope/Rope.cs
@@ -5,7 +5,11 @@ public class Rope : MonoBehaviour
     public Rigidbody2D Hook;
     public GameObject LinkPrefab;
     public int Links = 7;
-    public Weight weight;
+    public Rigidbody2D weight;
+    public float OffsetDistanceFromWeight;
+    public Vector2 WeightAnchor;
+    public bool AutoConfigureWeightConnectedAnchor = false;
+    public Vector2 WeightConnectedAnchor;
     
     void Start()
     {
@@ -19,15 +23,19 @@ public class Rope : MonoBehaviour
             GameObject link = Instantiate(LinkPrefab, transform);
             HingeJoint2D joint = link.GetComponent<HingeJoint2D>();
             joint.connectedBody = previousRB;
-            if (i < Links - 1)
-            {
-                previousRB = link.GetComponent<Rigidbody2D>();
-            }
-            else
-            {
-                if(weight) weight.ConnectToRopeEnd(link.GetComponent<Rigidbody2D>());
-            }
-                
+
+            previousRB = link.GetComponent<Rigidbody2D>();       
         }
+
+        if (!weight) return;
+        DistanceJoint2D lastJoint = weight.gameObject.AddComponent<DistanceJoint2D>();
+        if (!lastJoint) { Debug.LogError("Invalid weight object - there is no distance joint on it."); return; }
+
+        lastJoint.autoConfigureDistance = false;
+        lastJoint.autoConfigureConnectedAnchor = AutoConfigureWeightConnectedAnchor;
+        lastJoint.connectedBody = previousRB;
+        lastJoint.distance = OffsetDistanceFromWeight;
+        lastJoint.anchor = WeightAnchor;
+        lastJoint.connectedAnchor = WeightConnectedAnchor;
     }
 }
