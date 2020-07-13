14c830e0676f70a846173e39ac14f33049bc405f
NicolasIceberg <>
2020-07-13 22:17:47 +0300

    Improved PlayerController mechanics. Some assets now have default physics material assigned.

--- AdventureKinematics/Assets/Scripts/PlayerController/PlayerController.cs
+++ AdventureKinematics/Assets/Scripts/PlayerController/PlayerController.cs
@@ -4,53 +4,78 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float walkSpeed = 10.0f;
-    public float jumpHeight = 400.0f;
+    public float walkSpeed = 10f;
+    public float jumpHeight = 400f;
+    public float minJumpAirtime = 0.1f;
     public Vector2 movement;
 
     private Rigidbody2D rigBody;
     private CapsuleCollider2D CapsuleCollider;
-    private ContactPoint2D[] contactPoints = new ContactPoint2D[30];
-    private int contactPointsLength;
 
-    private Vector2 upVector = new Vector2(0,1);
-    private Vector2 forwardVector = new Vector2(1,0);
+    private const int contactPointsLength = 30;
+    private ContactPoint2D[] contactPoints = new ContactPoint2D[contactPointsLength];
+    
+    private float timeSinceLastJump;
+    private bool shouldJump = false;
+    private bool alreadyJumped = false;
 
     void Start()
     {
+        timeSinceLastJump = -minJumpAirtime - 1f;
         rigBody = this.GetComponent<Rigidbody2D>();
         CapsuleCollider = this.GetComponent<CapsuleCollider2D>();
     }
 
-    void Update()
+    private void Update()
     {
-        contactPointsLength = CapsuleCollider.GetContacts(contactPoints);
-        movement = new Vector2(Input.GetAxis("Horizontal"), 0);
-        if (Input.GetButtonDown("Jump"))
+        // ig you press jump button, then player should jump
+        if (Input.GetButton("Jump")) shouldJump = true;
+        else
         {
-            for (int i = 0; i < contactPointsLength; i++)
-            {
-                if (Vector2.Dot(upVector, contactPoints[i].normal) > 0)
-                {
-                    jump();
-                    break;
-                }
-            }
+            alreadyJumped = false;
+            shouldJump = false;         // set it to false, because if you release button, player then will jump if you press the next time
         }
     }
 
     void FixedUpdate()
     {
         moveCharacter(movement);
+        checkForJumping();
     }
+
 
     void moveCharacter(Vector2 direction)
     {
-        rigBody.AddForce((rigBody.mass * direction * walkSpeed) * ((walkSpeed - Mathf.Abs(Vector2.Dot(rigBody.velocity, forwardVector))) / walkSpeed ), ForceMode2D.Impulse);
+        movement = new Vector2(Input.GetAxis("Horizontal"), 0);
+
+        rigBody.AddForce((rigBody.mass * direction * walkSpeed) * ((walkSpeed - Mathf.Abs(Vector2.Dot(rigBody.velocity, transform.right))) / walkSpeed ), ForceMode2D.Impulse);
     }
 
-    void jump()
+    void checkForJumping()
     {
-        rigBody.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+        // get count of collisions, and collisions themselves, and also get maximal possible index of collisios
+        int n = CapsuleCollider.GetContacts(contactPoints);
+        n = Mathf.Min(contactPointsLength, n);
+
+        // if our player should jump, and it's not already jumped
+        if (shouldJump && !alreadyJumped)
+        {
+
+            // then check each object it's colliding with
+            for (int i = 0; i < n; i++)
+            {
+                // if the contact point is something like our legs, and not a head, and jump airtime is already longer than minimal acceptable, then:
+                if ((Vector2.Dot(transform.up, contactPoints[i].normal) > 0) && ((timeSinceLastJump - Time.time + minJumpAirtime) < 0f))
+                {
+                    // jump
+                    timeSinceLastJump = Time.time;
+                    rigBody.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
+                    alreadyJumped = true;
+
+                    // and break cycle to do not iterate further (for optimizing)
+                    break;
+                }
+            }
+        }
     }
 }
