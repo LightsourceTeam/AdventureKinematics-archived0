04c1c21b5f1d8a5f26d333c23c09279ffd2b8947
EarthRoy <>
2020-07-13 15:50:32 +0300

    Додано справжню версію замка.

---
+++ AdventureKinematics/Assets/Scripts/PhysicAssets/Lock.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lock : MonoBehaviour
+{
+    FixedJoint2D FJ;
+    public GameObject Key;
+ 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        FJ = GetComponent<FixedJoint2D>();
+        if (other.gameObject == Key)
+        {
+            Destroy(FJ);
+        }
+    }
+}
