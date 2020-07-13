37a76f3ac43e3630868a5ae1775ba77947d7f249
ElemRoy <>
2020-07-13 17:31:33 +0300

    Firethrower & Ice Object.

---
+++ AdventureKinematics/Assets/Scripts/PhysicAssets/Firethrower.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firethrower : MonoBehaviour
+{
+    public float LineLength;
+    public LayerMask layerMask;
+    public bool isOn = false;
+
+    private ParticleSystem Particles;
+    private bool particlesIsRunning;
+
+    void Start()
+    {
+        Particles = GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        if (isOn)
+        {
+                if (!particlesIsRunning)
+                {
+                    particlesIsRunning = true;
+                    Particles.Play(true);
+                }
+        }
+        else
+        {
+            Particles.Stop(true);
+            particlesIsRunning = false;
+        }
+    }
+}
