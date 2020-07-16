00d39d19552df69c06fa10cb1d71f203b012d1ce
ElemRoy <>
2020-07-16 17:43:38 +0300

    Tesla(Working) and Unfinished Laser commit.

--- AdventureKinematics/Assets/Scripts/PhysicAssets/Laser.cs
+++ AdventureKinematics/Assets/Scripts/PhysicAssets/Laser.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
-    public float LineLength = 5f;
+    public float LineLength;
     public Switch laserSwitch;
     public LayerMask layerMask;
+    public int reflectionCount;
 
+    private Ray2D ray;
     private RaycastHit2D hit;
+    private Vector2 currentReflectPosition;
+    private Vector2 direction;
     private LineRenderer Line;
 
     void Start()
     {
         Line = GetComponent<LineRenderer>();
-        Line.positionCount = 2;
         Line.enabled = false;
 
         Line.useWorldSpace = true;
@@ -33,19 +36,41 @@ public class Laser : MonoBehaviour
 
     private void Fire()
     {
-        
-        hit = Physics2D.Raycast(transform.position, transform.right, LineLength, layerMask);
-        
+        Line.positionCount = 1;
+        direction = transform.right;
+        currentReflectPosition = transform.position;
+        hit = Physics2D.Raycast(currentReflectPosition, direction, LineLength, layerMask);
         Line.enabled = true;
-        if (hit)
-        {
-            Line.SetPosition(0, transform.position);
-            Line.SetPosition(1, hit.point);
-            return;
-        }
+        float remainingDistance = LineLength;
+        int currentReflectionCount = 0;
 
-        Line.SetPosition(0, transform.position);
-        Line.SetPosition(1, ((Vector2)transform.right * LineLength) + (Vector2)transform.position);
+        do
+        {
+            if (currentReflectionCount > reflectionCount) break;
+            if (hit)
+            {
+                Line.positionCount += 1;
+                if (hit.transform.gameObject.tag != "Mirror") break;
+
+                Line.SetPosition(Line.positionCount - 2, currentReflectPosition);
+                Line.SetPosition(Line.positionCount - 1, currentReflectPosition + direction * remainingDistance);
+
+                direction = Quaternion.AngleAxis(180, hit.normal) * -direction;
+
+                remainingDistance -= Vector2.Distance(currentReflectPosition, hit.point);
+                if (remainingDistance < 0) break;
+
+                currentReflectPosition = hit.point;
+                currentReflectionCount++;
+
+                hit = Physics2D.Raycast(currentReflectPosition, direction, remainingDistance, layerMask);
+            }
+            else
+            {
+                Line.SetPosition(0, transform.position);
+                Line.SetPosition(1, transform.position + transform.right * LineLength);
+            }
+        } while (hit);
     }
 }
 
---
+++ AdventureKinematics/Assets/Scripts/PhysicAssets/Tesla.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tesla : MonoBehaviour
+{
+    public GameObject Main;
+    public GameObject Wait;
+    public Transform LinePosition;
+    public Switch TeslaSwitch;
+
+    private bool isParticles;
+    private bool isAudio;
+    private ParticleSystem Particles;
+    private AudioSource Audio;
+    private LineRenderer Line;
+    void Start()
+    {
+        Particles = GetComponent<ParticleSystem>();
+        Audio = GetComponent<AudioSource>();
+        Line = GetComponent<LineRenderer>();
+        Particles.Stop(true);
+        isParticles = false;
+        isAudio = false;
+    }
+
+    void Update()
+    {
+        if (TeslaSwitch.isChecked)
+        {
+            Line.SetPosition(0, LinePosition.transform.position);
+            Line.SetPosition(1, Wait.gameObject.transform.GetChild(0).transform.position);
+            Line.enabled = true;
+            if (!isParticles)
+            {
+                Particles.Play(true);
+                isParticles = true;
+            }
+            if (!isAudio)
+            {
+                Audio.Play();
+                isAudio = true;
+            }
+        }
+        else
+        {
+            Line.enabled = false;
+            Particles.Stop(true);
+            isParticles = false;
+            Audio.Stop();
+            isAudio = false;
+        }
+    }
+}
