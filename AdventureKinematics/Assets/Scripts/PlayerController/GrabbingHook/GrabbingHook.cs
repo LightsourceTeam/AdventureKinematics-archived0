5a851f64f089c1ff11678cd77776f56067c4577e
EarthRoy <>
2020-07-22 15:36:54 +0300

    Додано першу версію крюк кішки

---
+++ AdventureKinematics/Assets/Scripts/PlayerController/GrabbingHook/GrabbingHook.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabbingHook : MonoBehaviour
+{
+    [SerializeField] private PlayerController playercontroller;
+    [SerializeField] private float ClimbSpeed;
+    [SerializeField] private float DownClimbSpeed;
+    [SerializeField] private float MaxHookDistance;
+    [SerializeField] private HookRaycaster HookRaycaster;
+    [SerializeField] private DistanceJoint2D DistanceJoint;
+    [SerializeField] private HookRenderer HookRenderer;
+
+    private Camera camera;
+    private RaycastHit2D _hit;
+
+    public bool JointEnable => DistanceJoint.enabled;
+
+    private bool IsHooking;
+    private void Start()
+    {
+        camera = Camera.main;
+        DistanceJoint.enabled = false;
+    }
+
+    public void Climb()
+    {
+        EditJointDistance(-ClimbSpeed);
+    }
+    public void Descend()
+    {
+        EditJointDistance(DownClimbSpeed);
+    }
+    private void EditJointDistance(float delta)
+    {
+        DistanceJoint.distance += delta * Time.deltaTime;
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            CreateHook();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {   
+            Disable();
+        }
+        if (DistanceJoint.enabled)
+        {
+            HookRenderer.Render(transform.position, _hit.point);
+        }
+        if (JointEnable)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                Climb();
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                Descend();
+            }
+        }
+        if (IsHooking)
+        {
+            playercontroller.enabled = false;
+        }
+        else
+        {
+            playercontroller.enabled = true;
+        }
+    }
+    public void Disable()
+    {
+        IsHooking = false;
+        DistanceJoint.enabled = false;
+        HookRenderer.Disable();
+    }
+    public void CreateHook()
+    {
+        IsHooking = true;
+        Vector2 target = camera.ScreenToWorldPoint(Input.mousePosition);
+        _hit = HookRaycaster.Ray(transform.position, target, MaxHookDistance);
+        DistanceJoint.distance = Vector2.Distance(transform.position, _hit.point);
+
+        if (_hit.collider != null)
+        {
+            DistanceJoint.enabled = true;
+            DistanceJoint.connectedAnchor = _hit.point;
+            HookRenderer.Render(transform.position, _hit.point);
+        }
+    }
+}
---
+++ AdventureKinematics/Assets/Scripts/PlayerController/GrabbingHook/HookRaycaster.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookRaycaster : MonoBehaviour
+{
+    [SerializeField] private LayerMask mask;
+
+    public RaycastHit2D Ray(Vector2 orgin, Vector2 target, float distance)
+    {
+        return Physics2D.Raycast(orgin, target - orgin, distance, mask);
+    }
+}
