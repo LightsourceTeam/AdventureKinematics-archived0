caf4b1a709ecc2bf2a35f9634fd0c8a49f1b2e4e
EarthRoy <>
2020-07-30 14:21:38 +0300

    Додано м'яч,Lock,Weight,door оновлено player.prefab.

---
+++ AdventureKinematics/Assets/Scripts/PhysicAssets/Temp/TempLock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempLock : MonoBehaviour
+{
+    public GameObject Key;
+    Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.isKinematic = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == Key)
+        {
+            rb.isKinematic = false;
+        }
+    }
+}
---
+++ AdventureKinematics/Assets/Scripts/PhysicAssets/Weight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weight : MonoBehaviour
+{
+    public SwitchButton Button;
+    Rigidbody2D rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.isKinematic = true;
+    }
+    private void Update()
+    {
+        if (Button.isChecked == true)
+        {
+            rb.isKinematic = false;
+        }
+    }
+}
