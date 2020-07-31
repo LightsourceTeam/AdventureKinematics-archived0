f1dafa59a3b65a1075b24a4c0f0ad55de49dfa36
EarthRoy <>
2020-07-31 14:54:03 +0300

    Додано першу версію скрипта AntiGravity.

---
+++ AdventureKinematics/Assets/Scripts/PhysicAssets/AntiGravity.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntiGravity : MonoBehaviour
+{
+    public SwitchButton Button;
+
+    private float gr;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (Button.isChecked == true)
+        {
+            other.GetComponent<Rigidbody2D>().gravityScale *= -1; 
+            
+        }
+        
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.GetComponent<Rigidbody2D>().gravityScale < 0)
+        {
+            other.GetComponent<Rigidbody2D>().gravityScale *= -1;
+        }
+    }
+}
