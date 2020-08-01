a63b2f6c7e5a340f4ffe0cd264647539666149d8
NicolasIceberg <>
2020-08-01 13:54:41 +0300

    Minor Refactoring. Changes in Jetpack mechanics - fixed bugs.

---
+++ AdventureKinematics/Assets/Scripts/Interactables/Items/Jetpack.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.U2D.Path.GUIFramework;
+using UnityEngine;
+
+public class Jetpack : GameItem
+{
+    public float Strength;
+    public float MaxVelocity;
+
+    public override void OnFixedApply(Controller controller)
+    {
+        Vector2 flyForce = controller.joystick.Direction;
+        flyForce.x = (MaxVelocity * flyForce.x - controller.mainController.rigBody.velocity.x) / MaxVelocity;
+        flyForce.y = (Mathf.Max(MaxVelocity * flyForce.y, 0f) - Mathf.Max(controller.mainController.rigBody.velocity.y, 0f)) / MaxVelocity;
+        flyForce *= Strength;
+
+        controller.mainController.rigBody.AddForce(flyForce , ForceMode2D.Force);
+    }
+}
