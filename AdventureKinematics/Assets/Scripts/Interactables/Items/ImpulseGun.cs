6ba306e870f947af5ab99a59e0a58a6d45871f12
EarthRoy <>
2020-08-05 12:24:54 +0300

    Додано ImpulseGun.

---
+++ AdventureKinematics/Assets/Scripts/Interactables/Items/ImpulseGun.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpulseGun : GameItem
+{
+    public List<Collider2D> otherCollider;
+    private Collider2D coll;
+    private Controller controllerToUse;
+    
+    public float forse;
+    private void Start()
+    {
+        coll = GetComponent<Collider2D>();
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        otherCollider.Add(collision);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        otherCollider.Remove(collision);
+    }
+
+    public override void OnApply(Controller controller)
+    {
+        controllerToUse = controller;
+
+        Vector2 direction = controllerToUse.joystick.Direction;
+
+        foreach (Collider2D x in otherCollider)
+        {
+            if (x.GetComponent<Rigidbody2D>())
+            {
+                x.GetComponent<Rigidbody2D>().AddForce(direction * forse * Time.deltaTime, ForceMode2D.Impulse);
+            }
+        }
+    }
+}
