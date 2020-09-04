875161befb1bf330a6b7e3305f33713ac99785ef
ElemRoy <oleguk0509@gmail.com>
2020-09-04 21:45:59 +0300

    Server nearly complete.

---
+++ AdventureKinematics/Assets/Scripts/Networking/Server/Client.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System;
+
+
+namespace Server
+{
+    public class Client
+    {
+       
+        public static Client instance;
+        public Dictionary<byte, Channel> channels = new Dictionary<byte, Channel>();
+        
+        public int id;
+
+        private TcpClient tcp;
+        private NetworkStream stream;
+
+        // constructor for intitializing new client
+        public Client(TcpClient client)
+        {
+            tcp = client;
+        }
+
+        public void Connect()
+        {
+            // get tcp network stream
+            stream = tcp.GetStream();
+
+            // add default system channel
+            channels.Add(0, new Channel());
+
+            // begin data reading
+            stream.BeginRead(recvChannelBytes, 0, 1, Read, null);
+        }
+  
+        private byte[] recvChannelBytes = new byte[1];
+        private byte[] dataSizeBytes = new byte[4];
+        private void Read(IAsyncResult result)
+        {
+            // get channel to which we are reading
+            stream.EndRead(result);
+            byte channel = recvChannelBytes[0];
+
+
+            Channel currentChannel = channels[channel];
+            if (currentChannel.type == ChannelType.stream)
+            {
+                // receive data
+                byte[] data = new byte[currentChannel.readSize];
+                stream.Read(data, 0, currentChannel.readSize);
+                currentChannel.dataBuffer.Push(data);
+            }
+            else
+            {
+                // get data size
+                stream.Read(dataSizeBytes, 0, currentChannel.readSize);
+                int dataSize = BitConverter.ToInt32(dataSizeBytes, 0);
+                
+                // receive data
+                byte[] data = new byte[dataSize];
+                stream.Read(data, 0, dataSize);
+                currentChannel.dataBuffer.Push(data);
+            }
+
+            
+            // start waiting for the next data
+            stream.BeginRead(recvChannelBytes, 0, 1, Read, null);
+        }
+
+        private byte[] sendChannelBytes = new byte[1];
+        private void Write(byte[] data, int size, byte channel)
+        {
+            sendChannelBytes[0] = channel;
+            stream.Write(sendChannelBytes, 0, 1);
+            stream.Write(data, 0, size);
+        }
+
+    }
+
+    public enum ChannelType
+    {
+        json,
+        list,
+        stream
+    }
+    
+    public class Channel
+    {
+        public Channel() { }
+
+        public Stack<byte[]> dataBuffer = new Stack<byte[]>();
+        
+        public byte id = 0;
+        public int readSize = 0;
+        public ChannelType type = ChannelType.list;
+        
+    }
+
+}
---
+++ AdventureKinematics/Assets/Scripts/Networking/Server/Server.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System;
+
+namespace Server
+{
+    public class Server
+    {
+        public static Server server;
+
+        public static IPAddress ip = IPAddress.Parse("127.0.0.1");
+        public static int port = 23852;
+
+        public TcpListener tcpListener;
+        public Dictionary<int, Client> clients = new Dictionary<int, Client>();
+
+        public int lastId = 0;
+        
+        public void Start()
+        {
+            // setting active instance of the server
+            if(server == null) server = this;
+            else if(server != this) Debug.LogError(this + ": You can not have two servers run simultaneously!");
+            else Debug.LogWarning(this +": No need to set it as active server - it already has this value");
+
+            // initilaize tcp listener and start accepting clients
+            tcpListener = new TcpListener(ip, port);
+            tcpListener.Start();
+            tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+        }
+
+        public void TCPConnectCallback(IAsyncResult result)
+        {
+            // accept new client, and start listening for the new one.
+            TcpClient client = AcceptClient(result);
+
+            // alert that new client has connected
+            Debug.Log("Incoming Connection: " + client.Client.RemoteEndPoint);
+            
+            AddClient(new Client(client));
+
+            //Debug.Log("Connection Failed: " + client.Client.RemoteEndPoint);
+        }
+
+        public TcpClient AcceptClient(IAsyncResult result)
+        {
+            // accept new client, and start listening for the new one.
+            TcpClient client = tcpListener.EndAcceptTcpClient(result);
+            tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+
+            return client;
+        }
+        
+        public void AddClient(Client client)
+        {
+            clients.Add(lastId, client);
+            clients[lastId].Connect();
+            clients[lastId].id = lastId;
+            lastId++;
+        }
+
+        public void RemoveClient(Client client)
+        {
+            clients.Remove(client.id);
+        }
+
+    }
+}
